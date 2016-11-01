633e1ce4e32051205995e529af662e27a8d9bfb2
shotmk <shotmk@gmail.com>
2016-11-02 00:42:40 +0200

    New engine of json serializers registration introduced. From now every bean on type JsonSerializer registered in context will be added to object mapper.

---
+++ src/main/groovy/com/nikitab/domain/json/module/SerializerRegistererModuleBean.groovy
@@ -0,0 +1,31 @@
+package com.nikitab.domain.json.module
+
+import com.fasterxml.jackson.core.Version
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.module.SimpleSerializers
+import com.fasterxml.jackson.databind.Module.SetupContext
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class SerializerRegistererModuleBean extends SimpleModule {
+
+    private List<JsonSerializer> registeredSerializers
+
+    private SerializerRegistererModuleBean() {
+        super("DoctorSpecializationModule", new Version(1, 0, 0, "SNAPSHOT"))
+    }
+
+    @Autowired
+    public setDoctorSpecificationSerializer(List<JsonSerializer> registeredSerializers) {
+        this.registeredSerializers = registeredSerializers
+    }
+
+    @Override
+    void setupModule(SetupContext context) {
+        SimpleSerializers serializers = new SimpleSerializers()
+		serializers.addSerializers(registeredSerializers)
+        context.addSerializers(serializers)
+    }
+}
